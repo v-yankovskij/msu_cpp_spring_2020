ae864b2162d96e68dad0195276cb13625bbc43d4
v-yankovskij <48069266+v-yankovskij@users.noreply.github.com>
2020-03-23 16:18:16 +0300

    Update main.cpp

--- 01/main.cpp
+++ 01/main.cpp
@@ -1,1 +1,28 @@
+#include "my_allocator.h"
 
+int main(int argc, char* argv[])
+{
+    size_t max_size = (size_t)std::stoi(argv[1], nullptr);
+    my_allocator allocator(max_size);
+    for(int i = 2; i < argc; i++)
+    {
+        if (argv[i][0] == 'r')
+        {
+            allocator.reset();
+        }
+        else
+        {
+            size_t size = (size_t)std::stoi(argv[i], nullptr);
+            char* ptr = allocator.alloc(size);
+            if (ptr == nullptr)
+            {
+                std::cout << "out of memory ";
+            }
+            else
+            {
+                std::cout << "allocated ";
+            }
+        }
+    }
+    return 0;
+}
