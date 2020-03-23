5c7420f6f80cd0c2faf111a223228e7eeaa207c4
v-yankovskij <48069266+v-yankovskij@users.noreply.github.com>
2020-03-23 16:22:38 +0300

    Update test.py

--- 01/test.py
+++ 01/test.py
@@ -8,3 +8,6 @@ def check(proc, expr, result):
         print('Got:' out.stdout)
     else:
         print(expr, ' - correct')
+        
+check('./LinearAllocator', '100 25 50 75 r 90', 'allocated allocated out of memory allocated ')
+check('./LinearAllocator', '15 100 4 9 4 r 4', 'out of memory allocated allocated out of memory allocated')
