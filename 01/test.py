e65a8b625707dd82a6ca164f43c4457b91e00c92
v-yankovskij <48069266+v-yankovskij@users.noreply.github.com>
2020-03-23 16:23:18 +0300

    Update test.py

--- 01/test.py
+++ 01/test.py
@@ -9,5 +9,5 @@ def check(proc, expr, result):
     else:
         print(expr, ' - correct')
         
-check('./LinearAllocator', '100 25 50 75 r 90', 'allocated allocated out of memory allocated ')
-check('./LinearAllocator', '15 100 4 9 4 r 4', 'out of memory allocated allocated out of memory allocated')
+check('./MyAllocator', '100 25 50 75 r 90', 'allocated allocated out of memory allocated ')
+check('./MyAllocator', '15 100 4 9 4 r 4', 'out of memory allocated allocated out of memory allocated')
