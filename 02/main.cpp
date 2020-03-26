05fa140f0d67cde62d10ececb01ae90dcd1134e8
v-yankovskij <48069266+v-yankovskij@users.noreply.github.com>
2020-03-26 16:27:25 +0300

    Update main.cpp

--- 02/main.cpp
+++ 02/main.cpp
@@ -33,7 +33,11 @@ int main(int argc, char* argv[])
         s.append(argv[i]);
         s.append(" ");
     }
-    parser p(callback_on_number, callback_on_string, callback_on_start, callback_on_stop);
+    parser p;
+    p.define_callback_on_number(callback_on_number);
+    p.define_callback_on_string(callback_on_string);
+    p.define_callback_on_start(callback_on_start);
+    p.define_callback_on_stop(callback_on_stop);
     p.parse(s);
     return 0;
 }
