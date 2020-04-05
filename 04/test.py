d0e76d5661dc8d15c5ce5f26087b6260c3469f48
v-yankovskij <48069266+v-yankovskij@users.noreply.github.com>
2020-04-05 17:29:55 +0300

    Create test.py

---
+++ 04/test.py
@@ -0,0 +1,13 @@
+import subprocess
+
+def check(proc, expr, result):
+    out = subprocess.run([proc, expr], stdout=subprocess.PIPE)
+    if out.stdout.decode('ascii') != result:
+        print(expr, ' - error')
+        print('Expected: ', result)
+        print('Got:' out.stdout)
+    else:
+        print(expr, ' - correct')
+        
+check('./MyAllocator', '100 25 50 75 r 90', 'allocated allocated out of memory allocated ')
+check('./MyAllocator', '15 100 4 9 4 r 4', 'out of memory allocated allocated out of memory allocated ')
