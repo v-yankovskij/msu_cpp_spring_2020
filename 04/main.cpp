b7f930444a259b279b6d38658dd246e652ae0652
v-yankovskij <48069266+v-yankovskij@users.noreply.github.com>
2020-04-05 20:54:22 +0300

    Update main.cpp

--- 04/main.cpp
+++ 04/main.cpp
@@ -13,12 +13,15 @@ struct Data
 };
 
 
-int main(int argc, char* argv[];)
+int main(int argc, char* argv[])
 {
-    Data x{argv[0][0], (std::stoi(argv[1]) > 0), std::stoi(argv[2])};
+    std::stringstream stream;
+	Serializer serializer(stream);
+	Deserializer deserializer(stream);
+    Data x{argv[1][0], (std::stoi(argv[2]) > 0), std::stoi(argv[3])};
     Data y{'c', false, 1};
     serializer.save(x);
     const Error err = deserializer.load(y);
-    std::cout << (x.a == y.a) << ' ' << assert(x.b == y.b) << ' ' << assert(x.c == y.c);
+    std::cout << (x.a == y.a) << ' ' << (x.b == y.b) << ' ' << (x.c == y.c);
 }
 
