24657704179ada3ad1d77982ce843ea3020ba81c
v-yankovskij <48069266+v-yankovskij@users.noreply.github.com>
2020-04-09 13:45:16 +0300

    Update main.cpp

--- 05/main.cpp
+++ 05/main.cpp
@@ -1,4 +1,4 @@
-#include <bigint.h>
+#include "bigint.h"
 #include <stdio.h>
 
 int main(int argc, char* argv[])
@@ -6,4 +6,5 @@ int main(int argc, char* argv[])
     BigInt a = BigInt(int64_t(std::stoi(argv[1])));
     BigInt b = BigInt(int64_t(std::stoi(argv[2])));
     cout << a + b << ' ' << a - b << ' ' << a * b;
+    return 0;
 }
