47323a7b91f10349f63e46cd17055a90b3932d85
v-yankovskij <48069266+v-yankovskij@users.noreply.github.com>
2020-04-09 14:25:23 +0300

    Update main.cpp

--- 05/main.cpp
+++ 05/main.cpp
@@ -5,6 +5,6 @@ int main(int argc, char* argv[])
 {
     BigInt a = BigInt(int64_t(std::stoi(argv[1])));
     BigInt b = BigInt(int64_t(std::stoi(argv[2])));
-    cout << a + b << ' ' << a - b << ' ' << a * b;
+    std::cout << a + b << ' ' << a * b;
     return 0;
 }
