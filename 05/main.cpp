f2d0bd862f0f70fd80b99db77e58371b88347b12
v-yankovskij <48069266+v-yankovskij@users.noreply.github.com>
2020-04-09 13:44:56 +0300

    Update main.cpp

--- 05/main.cpp
+++ 05/main.cpp
@@ -1,9 +1,9 @@
 #include <bigint.h>
 #include <stdio.h>
 
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
-    BigInt a = BigInt(atoi(argv[0]))
-    BigInt b = BigInt(atoi(argv[0]))
-    cout << a + b << ' ' << a - b << ' ' << a * b 
+    BigInt a = BigInt(int64_t(std::stoi(argv[1])));
+    BigInt b = BigInt(int64_t(std::stoi(argv[2])));
+    cout << a + b << ' ' << a - b << ' ' << a * b;
 }
