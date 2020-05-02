f3fcff270ba0963d9c3d5f4176515ce2a78c65b6
v-yankovskij <48069266+v-yankovskij@users.noreply.github.com>
2020-05-02 15:55:33 +0300

    Create test.py

---
+++ 06/test.py
@@ -0,0 +1,12 @@
+import subprocess
+
+def check(proc, expr, result):
+    out = subprocess.run([proc, expr], stdout=subprocess.PIPE)
+    if out.stdout.decode('ascii') != result:
+        print(expr, ' - error')
+        print('Expected: ', result)
+        print('Got:' out.stdout)
+    else:
+        print(expr, ' - correct')
+        
+check('./BigInt', '2 2', '4 4')
