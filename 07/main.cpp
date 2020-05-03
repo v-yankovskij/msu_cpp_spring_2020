4de45a11aa524fd528b7081b363b6b2f590832b2
v-yankovskij <48069266+v-yankovskij@users.noreply.github.com>
2020-05-03 10:58:39 +0300

    Create main.cpp

---
+++ 07/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "vector.h"
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    Vector<int> v;
+    for(int i = 1; i < argc; i++)
+    {
+        v.push_back(std::stoi(argv[i]));
+    }
+    for(int i = 0; i < argc - 1; i++)
+    {
+        std::cout << v[i] << " ";
+    }
