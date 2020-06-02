dfded1cdfc0ea129f012f067f9956f4622b07bae
v-yankovskij <48069266+v-yankovskij@users.noreply.github.com>
2020-06-02 12:47:06 +0300

    Create main.cpp

---
+++ 09/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdio.h>
+#include "sort.h"
+
+constexpr auto BufferSize = 8 << 20;
+
+int main(int argc, char* argv[])
+{
+    std::vector<uint64_t> elements;
+    for (i = 1; i < argc; i++)
+    {
+        elements.push_back(std::stoi(argv[i]));
+    }
+    std::ofstream input_file("input.txt", std::ios::binary | std::ios::trunc);
+    for (auto& elem : elements)
+    {
+        file.write(reinterpret_cast<char*>(&elem), sizeof(elem));
+    }
+    auto path = std::experimental::filesystem::path("input.txt");
+    auto save_path = std::experimental::filesystem::path("output.txt");
+    FileSorter Sorter(BufferSize);
+    Sorter.Run(path.string(), save_path.string());
+    std::ofstream output_file("input.txt", std::ios::binary | std::ios::trunc);
+    return 0;
+}
