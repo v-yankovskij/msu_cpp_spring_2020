d461d883ecf54e961fd425cc3914678dbb4ad125
v-yankovskij <48069266+v-yankovskij@users.noreply.github.com>
2020-06-11 18:38:19 +0300

    Update test.py

--- exam/test.py
+++ exam/test.py
@@ -1,1 +1,14 @@
+import subprocess
 
+def check(proc, expr, result):
+    out = subprocess.run([proc, expr], stdout=subprocess.PIPE)
+    if out.stdout.decode('ascii') != result:
+        print(expr, ' - error')
+        print('Expected: ', result)
+        print('Got:' out.stdout)
+    else:
+        print(expr, ' - correct')
+        
+check('./Exam', 'file1.txt 2', '111')
+check('./Exam', 'file2.txt 2', '31')
+check('./Exam', 'file3.txt 2', '92')
